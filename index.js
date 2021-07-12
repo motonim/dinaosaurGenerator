d8825937c92662d2e675a25b8fa33d6538f4a148
Jaeri Park <motonimjrp@gmail.com>
2021-07-12 19:42:34 -0400

    fetching dinosaure images

--- index.js
+++ index.js
@@ -24,3 +24,19 @@ app.get('/dinoname', async (request, response) => {
   console.log(dinoNameResponse);
   response.json(dinoNameResponse);
 });
+
+app.get('/dinoimage', async (request, response) => {
+  const fetchApi = await fetch(
+    'https://bing-image-search1.p.rapidapi.com/images/search?q=dinosaur&count=10',
+    {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-key': '4d91f8a78bmsh1e1849ffdcfd645p1859f2jsnf0c7106f344a',
+        'x-rapidapi-host': 'bing-image-search1.p.rapidapi.com',
+      },
+    }
+  );
+  const dinoImageResponse = await fetchApi.json();
+  console.log(dinoImageResponse);
+  response.json(dinoImageResponse);
+});
--- public/script.js
+++ public/script.js
@@ -2,6 +2,7 @@ console.log('script.js loaded');
 
 document.querySelector('#btnLoad').addEventListener('click', () => {
   getDinoName();
+  getDinoImage();
 });
 
 async function getDinoName() {
@@ -9,4 +10,12 @@ async function getDinoName() {
   const data = await response.json();
   let dinoName = data[0].join(' ');
   console.log(dinoName);
+}
+
+async function getDinoImage() {
+  const response = await fetch('/dinoimage');
+  const data = await response.json();
+  let dinoImage =
+    data.value[Math.floor(Math.random() * data.value.length)].thumbnailUrl;
+  console.log(dinoImage);
 }

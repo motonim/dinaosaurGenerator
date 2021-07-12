44e029bc751b8ac2e0bf6c101d0da2bfd3c44ea1
Jaeri Park <motonimjrp@gmail.com>
2021-07-12 17:17:35 -0400

    adding button to generate dinosaur names

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const fetch = require('node-fetch');
 
 app.use(express.static('public'));
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.get('/dinoname', async (request, response) => {
+  const fetchApi = await fetch(
+    'https://alexnormand-dino-ipsum.p.rapidapi.com/?paragraphs=1&words=2&format=json',
+    {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-key': '4d91f8a78bmsh1e1849ffdcfd645p1859f2jsnf0c7106f344a',
+        'x-rapidapi-host': 'alexnormand-dino-ipsum.p.rapidapi.com',
+      },
+    }
+  );
+  const dinoNameResponse = await fetchApi.json();
+  console.log(dinoNameResponse);
+  response.json(dinoNameResponse);
 });
--- public/script.js
+++ public/script.js
@@ -1,1 +1,12 @@
-console.log('script.js loaded')
+console.log('script.js loaded');
+
+document.querySelector('#btnLoad').addEventListener('click', () => {
+  getDinoName();
+});
+
+async function getDinoName() {
+  const response = await fetch('/dinoname');
+  const data = await response.json();
+  let dinoName = data[0].join(' ');
+  console.log(dinoName);
+}
